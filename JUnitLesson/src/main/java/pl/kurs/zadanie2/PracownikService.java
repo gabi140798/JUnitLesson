66e46f33b04fac1505bbe5ed103790498644a701
Gabrysia Gołąb <gabriela.golab.1998@gmail.com>
2023-10-25 17:26:15 +0200

    initial commit

--- JUnitLesson/src/main/java/pl/kurs/zadanie2/PracownikService.java
+++ JUnitLesson/src/main/java/pl/kurs/zadanie2/PracownikService.java
@@ -1,8 +1,5 @@
 package pl.kurs.zadanie2;
 
-import java.util.Collections;
-import java.util.Objects;
-import java.util.Optional;
 
 public class PracownikService {
 
--- JUnitLesson/src/test/java/pl/kurs/zadanie2/FirmaTest.java
+++ JUnitLesson/src/test/java/pl/kurs/zadanie2/FirmaTest.java
@@ -12,14 +12,34 @@ public class FirmaTest {
 
     @Before
     public void setup() {
+        Pracownik.getPracownicy().clear();
+        Pracownik p1 = new Pracownik("Adam", "Kowalski", 10000);
+        Pracownik p2 = new Pracownik("Katarzyna", "Nowak", 4000);
         f1 = new Firma("Firma");
         fs1 = new FirmaService();
+        f1.dodajPracownika(p1);
+        f1.dodajPracownika(p2);
 
     }
 
     @Test
     public void sprawdzCzyDlugoscNazwyFirmySieZgadza() {
         assertEquals(5,f1.getNazwa().length());
+    }
+
+    @Test
+    public void czyPensjaWszystkichPracownikowSieZgadza() {
+        assertEquals(14000,0,fs1.wyplataDlaWszystkichPracownikow(f1));
+    }
+
+    @Test
+    public void sprawdzaCzyEkstensjaWynosi2() {
+        assertEquals(2, f1.getListaPracownikow().size());
+    }
+
+    @Test
+    public void sprawdzaCzyEkstensjaWynosi3() {
+        assertEquals(3, f1.getListaPracownikow().size());
     }
 
 }
